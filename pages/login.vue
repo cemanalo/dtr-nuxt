815dabd5d629764971cd379e547de3fff9c125bc
Carlo Eugene Manalo <carloeugene@gmail.com>
2018-10-11 01:06:11 +0800

    Initial commit vuetify

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <v-card>
+        <v-card-title class="headline">Login form</v-card-title>
+        <v-form>
+          <v-card-text>
+            <v-text-field
+              label="Email"
+            />
+            <v-text-field
+              label="Password"
+              type="password"
+            />
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer/>
+            <v-btn
+              color="primary"
+              flat
+              nuxt
+            >Login</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  layout: 'clean'
+}
+</script>
