698cec26315172434bc9fcdde143c09c67483a7f
Carlo Eugene Manalo <cemanalo@cambridge.org>
2018-12-13 19:52:36 +0800

    Fix veutify data table searchlimitation

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,11 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
     ]
   },
 
@@ -27,9 +31,7 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-    '~/assets/style/app.styl'
-  ],
+  css: ['~/assets/style/app.styl'],
 
   /*
   ** Plugins to load before mounting the App
--- pages/employees/index.vue
+++ pages/employees/index.vue
@@ -5,20 +5,32 @@
     align-center>
     <v-flex
       xs12>
+      <v-card-title>
+        Employees
+        <v-spacer/>
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+        />
+      </v-card-title>
       <v-data-table 
         :headers="headers"
         :items="employees"
-        :total-items="totalItems"
+        :total-items="totalEmployees"
         :loading="loading"
         :pagination.sync="pagination"
+        class="elevation-1"
       >
         <template 
           slot="items" 
           slot-scope="props">
-          <td>{{ props.item.workdayId }}</td>
-          <td>{{ props.item.lastName }}</td>
-          <td>{{ props.item.firstName }}</td>
-          <td>{{ props.item.position }}</td>
+          <td>{{ props.item._source.workdayId }}</td>
+          <td>{{ props.item._source.lastName }}</td>
+          <td>{{ props.item._source.firstName }}</td>
+          <td>{{ props.item._source.position }}</td>
           <td>Action</td>
         </template>
       </v-data-table>
@@ -29,6 +41,8 @@
 <script>
 import headers from './headers.json'
 import to from 'await-to-js'
+import _ from 'lodash'
+import { setTimeout } from 'timers'
 
 export default {
   data() {
@@ -37,36 +51,66 @@ export default {
       pagination: {},
       loading: true,
       employees: [],
-      totalItems: 0
+      totalEmployees: 0,
+      search: ''
     }
   },
   watch: {
     pagination: {
-      async handler() {
+      handler() {
         this.loading = true
+        const self = this
         const pageReq = this.pagination
-        const limit = pageReq.rowsPerPage
-        const skip = limit * (pageReq.page - 1)
+        const size = pageReq.rowsPerPage
+        const from = size * (pageReq.page - 1)
 
-        const filter = JSON.stringify({
-          limit,
-          skip
-        })
+        this.$axios
+          .$get(
+            `/api/employees/search?from=${from}&size=${size}&q=${this.search}`
+          )
+          .then(employees => {
+            this.loading = false
+            this.employees = _.get(employees, 'result.hits.hits', [])
+            this.totalEmployees = _.get(employees, 'result.hits.total', 0)
+          })
+      },
+      deep: true
+    },
+    search: {
+      handler() {
+        this.loading = true
+        const self = this
+        const pageReq = this.pagination
+        const size = pageReq.rowsPerPage
+        const from = size * (pageReq.page - 1)
 
-        const [employeesErr, employees] = await to(
-          this.$axios.$get(`/api/employees?filter=${filter}`)
-        )
-
-        const [totalItemsErr, totalItems] = await to(
-          this.$axios.$get(`/api/employees/count`)
-        )
-
-        this.loading = false
-        this.employees = employees
-        this.totalItems = totalItems.count
+        this.$axios
+          .$get(
+            `/api/employees/search?from=0&size=${size}&q=${this.search || '*'}`
+          )
+          .then(employees => {
+            this.loading = false
+            this.employees = _.get(employees, 'result.hits.hits', [])
+            this.totalEmployees = _.get(employees, 'result.hits.total', 0)
+          })
       },
       deep: true
     }
+  },
+  mounted() {
+    this.loading = true
+    const self = this
+    const pageReq = this.pagination
+    const size = pageReq.rowsPerPage
+    const from = size * (pageReq.page - 1)
+
+    this.$axios
+      .$get(`/api/employees/search?from=${from}&size=${size}&q=${this.search}`)
+      .then(employees => {
+        this.loading = false
+        this.employees = _.get(employees, 'result.hits.hits', [])
+        this.totalEmployees = _.get(employees, 'result.hits.total', 0)
+      })
   }
 }
 </script>
